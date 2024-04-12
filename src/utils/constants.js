8b863d6091db5ddee79599f3cb6a80fe468e7eef
Jaydeep Godhani <godhani.rameshbhai@walmart.com>
2024-04-13 00:55:53 +0530

    routing working

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyCo4AgI-smpkCnGew32QVuc_5i9Q-XxLL0";
+const API2_KEY = "AIzaSyCo4AgI-smpkCnGew32QVuc_5i9Q-XxLL0";
 
 export const POPULAR_VIDEOS =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&regionCode=in&key=" + API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&regionCode=in&key=" + API2_KEY;
