0a03be24ac826a1cd1fb13166f1b6a76715a75cc
Jaydeep Godhani <godhani.rameshbhai@walmart.com>
2024-04-22 01:14:16 +0530

    live chat added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
-const API2_KEY = "your_key";
+const API2_KEY = "keyy";
 
 export const POPULAR_VIDEOS =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&regionCode=in&key=" + API2_KEY;
 
 export const SEARCH_SUGGETION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const LIVE_CHAT_COUNT = 25;
